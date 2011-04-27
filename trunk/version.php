30c706cc2962a3d3c59b2279ab1b40b0e0251161
kovpas@gmail.com <kovpas@gmail.com@f4410f8d-abd1-10e1-d7bc-84c51b6d3af0>
2011-04-27 09:13:26 +0000

    Added php version of version.txt

---
+++ trunk/version.php
@@ -0,0 +1,7 @@
+<?php
+    header('Access-Control-Allow-Origin: http://butsa.ru');
+    header('Access-Control-Allow-Origin: http://www.butsa.ru');
+    header('Access-Control-Allow-Origin: http://champions.ru');
+    header('Access-Control-Allow-Origin: http://www.champions.ru');
+    echo('0.0.5');
+?>
