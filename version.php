48fdec974b39e52912cec3f9ce545ff3a7a455ef
kovpas <kovpas@gmail.com>
2011-04-27 09:45:16 +0000

    Removed version.php

--- version.php
+++
@@ -1,7 +0,0 @@
-<?php
-    header('Access-Control-Allow-Origin: http://butsa.ru');
-    header('Access-Control-Allow-Origin: http://www.butsa.ru');
-    header('Access-Control-Allow-Origin: http://champions.ru');
-    header('Access-Control-Allow-Origin: http://www.champions.ru');
-    echo('0.0.5');
-?>
